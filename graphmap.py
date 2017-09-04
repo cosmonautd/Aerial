6bd1a357b8181750865da80d8b22f6e9b191d5c2
dave <david.borges@protonmail.com>
2017-09-04 21:26:42 +0000

    Adding test code for weight

--- graphmap.py
+++ graphmap.py
@@ -17,26 +17,33 @@ print(framematrix.shape)
 
 G = graphtool.Graph(directed=False)
 pos = G.new_vertex_property("vector<double>")
+diff = G.new_vertex_property("vector<double>")
+weight = G.new_edge_property("double")
 
 for i, row in enumerate(framematrix):
     for j, element in enumerate(row):
         v = G.add_vertex()
         pos[v] = [i, j]
+        diff[v] = framematrix[i][j]
 
 for v in G.vertices():
     (i, j) = pos[v][0], pos[v][1]
     top, bottom, left, right = (i-1, j), (i+1, j), (i, j-1), (i, j+1)
     if i-1 > -1:
         if G.edge(coord2(top, framematrix.shape[1]), v) == None:
-            G.add_edge(v, G.vertex(coord2(top, framematrix.shape[1])))
+            e = G.add_edge(v, G.vertex(coord2(top, framematrix.shape[1])))
+            weight[e] = abs(diff[v] - diff[G.vertex(coord2(top, framematrix.shape[1]))])
     if i+1 < framematrix.shape[0]:
         if G.edge(coord2(bottom, framematrix.shape[1]), v) == None:
-            G.add_edge(v, G.vertex(coord2(bottom, framematrix.shape[1])))
+            e = G.add_edge(v, G.vertex(coord2(bottom, framematrix.shape[1])))
+            weight[e] = abs(diff[v] - diff[G.vertex(coord2(bottom, framematrix.shape[1]))])
     if j-1 > -1:
         if G.edge(coord2(left, framematrix.shape[1]), v) == None:
-            G.add_edge(v, G.vertex(coord2(left, framematrix.shape[1])))
+            e = G.add_edge(v, G.vertex(coord2(left, framematrix.shape[1])))
+            weight[e] = abs(diff[v] - diff[G.vertex(coord2(left, framematrix.shape[1]))])
     if j+1 < framematrix.shape[1]:
         if G.edge(coord2(right, framematrix.shape[1]), v) == None:
-            G.add_edge(v, G.vertex(coord2(right, framematrix.shape[1])))
+            e = G.add_edge(v, G.vertex(coord2(right, framematrix.shape[1])))
+            weight[e] = abs(diff[v] - diff[G.vertex(coord2(right, framematrix.shape[1]))])
 
 draw.graph_draw(G, pos=pos, output_size=(1000, 1000), output="tdg.png")
