2720af15cfb0e553c6fb9a02ace34678ef1f8463
Dave <david.borges@protonmail.com>
2017-09-17 00:12:55 -0300

    Implementing tdm2graph, configuring graph internal properties

--- graphmap.py
+++ graphmap.py
@@ -40,8 +40,71 @@ class RouteEstimator:
     def __init__(self):
         pass
     
-    def tdi2graph(self, tdmatrix):
-        pass
+    def tdm2graph(self, tdmatrix):
+
+        G = graphtool.Graph(directed=False)
+
+        G.vp.pos = G.new_vertex_property("vector<double>")
+        G.vp.pos2 = G.new_vertex_property("vector<double>")
+        G.vp.diff = G.new_vertex_property("double")
+        G.ep.weight = G.new_edge_property("double")
+
+        for i, row in enumerate(tdmatrix):
+            for j, element in enumerate(row):
+                v = G.add_vertex()
+                G.vp.pos[v] = [i, j]
+                G.vp.pos2[v] = [j, i]
+                G.vp.diff[v] = tdmatrix[i][j]
+
+        for v in G.vertices():
+            (i, j) = G.vp.pos[v][0], G.vp.pos[v][1]
+
+            top, bottom, left, right = (i-1, j), (i+1, j), (i, j-1), (i, j+1)
+            if i-1 > -1:
+                if G.edge(coord2(top, tdmatrix.shape[1]), v) == None:
+                    e = G.add_edge(v, G.vertex(coord2(top, tdmatrix.shape[1])))
+                    G.ep.weight[e] = (G.vp.diff[v] + G.vp.diff[G.vertex(coord2(top, tdmatrix.shape[1]))])
+            if i+1 < tdmatrix.shape[0]:
+                if G.edge(coord2(bottom, tdmatrix.shape[1]), v) == None:
+                    e = G.add_edge(v, G.vertex(coord2(bottom, tdmatrix.shape[1])))
+                    G.ep.weight[e] = (G.vp.diff[v] + G.vp.diff[G.vertex(coord2(bottom, tdmatrix.shape[1]))])
+            if j-1 > -1:
+                if G.edge(coord2(left, tdmatrix.shape[1]), v) == None:
+                    e = G.add_edge(v, G.vertex(coord2(left, tdmatrix.shape[1])))
+                    G.ep.weight[e] = (G.vp.diff[v] + G.vp.diff[G.vertex(coord2(left, tdmatrix.shape[1]))])
+            if j+1 < tdmatrix.shape[1]:
+                if G.edge(coord2(right, tdmatrix.shape[1]), v) == None:
+                    e = G.add_edge(v, G.vertex(coord2(right, tdmatrix.shape[1])))
+                    G.ep.weight[e] = (G.vp.diff[v] + G.vp.diff[G.vertex(coord2(right, tdmatrix.shape[1]))])
+            
+            topleft, topright, bottomleft, bottomright = (i-1, j-1), (i-1, j+1), (i+1, j-1), (i+1, j+1)
+            if i-1 > -1 and j-1 > -1:
+                if G.edge(coord2(topleft, tdmatrix.shape[1]), v) == None:
+                    e = G.add_edge(v, G.vertex(coord2(topleft, tdmatrix.shape[1])))
+                    G.ep.weight[e] = (G.vp.diff[v] + G.vp.diff[G.vertex(coord2(topleft, tdmatrix.shape[1]))])
+            if i-1 > -1 and j+1 < tdmatrix.shape[1]:
+                if G.edge(coord2(topright, tdmatrix.shape[1]), v) == None:
+                    e = G.add_edge(v, G.vertex(coord2(topright, tdmatrix.shape[1])))
+                    G.ep.weight[e] = (G.vp.diff[v] + G.vp.diff[G.vertex(coord2(topright, tdmatrix.shape[1]))])
+            if i+1 < tdmatrix.shape[0] and j-1 > -1:
+                if G.edge(coord2(bottomleft, tdmatrix.shape[1]), v) == None:
+                    e = G.add_edge(v, G.vertex(coord2(bottomleft, tdmatrix.shape[1])))
+                    G.ep.weight[e] = (G.vp.diff[v] + G.vp.diff[G.vertex(coord2(bottomleft, tdmatrix.shape[1]))])
+            if i+1 < tdmatrix.shape[0] and j+1 < tdmatrix.shape[1]:
+                if G.edge(coord2(bottomright, tdmatrix.shape[1]), v) == None:
+                    e = G.add_edge(v, G.vertex(coord2(bottomright, tdmatrix.shape[1])))
+                    G.ep.weight[e] = (G.vp.diff[v] + G.vp.diff[G.vertex(coord2(bottomright, tdmatrix.shape[1]))])
+
+        G.vp.vfcolor = G.new_vertex_property("vector<double>")
+        G.ep.ecolor = G.new_edge_property("vector<double>")
+        G.ep.ewidth = G.new_edge_property("int")
+        for v in G.vertices():
+            G.vp.vfcolor[v] = [0, 0.0, 0.0, 1.0]
+        for e in G.edges():
+            G.ep.ewidth[e] = G.ep.weight[e]/16 + 1
+            G.ep.ecolor[e] = [0.179, 0.203, 0.210, 0.8]
+        
+        return G
     
     def route(self):
         pass
@@ -53,85 +116,29 @@ tdigenerator = gtde.GroundTraversalDifficultyEstimator( \
 frame = gtde.loadimage('img/aerial2.jpg')
 tdmatrix = tdigenerator.computematrix(frame)
 
-G = graphtool.Graph(directed=False)
-pos = G.new_vertex_property("vector<double>")
-pos2 = G.new_vertex_property("vector<double>")
-diff = G.new_vertex_property("double")
-weight = G.new_edge_property("double")
+router = RouteEstimator()
+G = router.tdm2graph(tdmatrix)
 
-for i, row in enumerate(tdmatrix):
-    for j, element in enumerate(row):
-        v = G.add_vertex()
-        pos[v] = [i, j]
-        pos2[v] = [j, i]
-        diff[v] = tdmatrix[i][j]
+draw.graph_draw(G, pos=G.vp.pos2, output_size=(1200, 1200), vertex_fill_color=G.vp.vfcolor,\
+                edge_color=G.ep.ecolor, edge_pen_width=G.ep.ewidth, output="tdg.png")
 
-for v in G.vertices():
-    (i, j) = pos[v][0], pos[v][1]
-
-    top, bottom, left, right = (i-1, j), (i+1, j), (i, j-1), (i, j+1)
-    if i-1 > -1:
-        if G.edge(coord2(top, tdmatrix.shape[1]), v) == None:
-            e = G.add_edge(v, G.vertex(coord2(top, tdmatrix.shape[1])))
-            weight[e] = (diff[v] + diff[G.vertex(coord2(top, tdmatrix.shape[1]))])
-    if i+1 < tdmatrix.shape[0]:
-        if G.edge(coord2(bottom, tdmatrix.shape[1]), v) == None:
-            e = G.add_edge(v, G.vertex(coord2(bottom, tdmatrix.shape[1])))
-            weight[e] = (diff[v] + diff[G.vertex(coord2(bottom, tdmatrix.shape[1]))])
-    if j-1 > -1:
-        if G.edge(coord2(left, tdmatrix.shape[1]), v) == None:
-            e = G.add_edge(v, G.vertex(coord2(left, tdmatrix.shape[1])))
-            weight[e] = (diff[v] + diff[G.vertex(coord2(left, tdmatrix.shape[1]))])
-    if j+1 < tdmatrix.shape[1]:
-        if G.edge(coord2(right, tdmatrix.shape[1]), v) == None:
-            e = G.add_edge(v, G.vertex(coord2(right, tdmatrix.shape[1])))
-            weight[e] = (diff[v] + diff[G.vertex(coord2(right, tdmatrix.shape[1]))])
-    
-    topleft, topright, bottomleft, bottomright = (i-1, j-1), (i-1, j+1), (i+1, j-1), (i+1, j+1)
-    if i-1 > -1 and j-1 > -1:
-        if G.edge(coord2(topleft, tdmatrix.shape[1]), v) == None:
-            e = G.add_edge(v, G.vertex(coord2(topleft, tdmatrix.shape[1])))
-            weight[e] = (diff[v] + diff[G.vertex(coord2(topleft, tdmatrix.shape[1]))])
-    if i-1 > -1 and j+1 < tdmatrix.shape[1]:
-        if G.edge(coord2(topright, tdmatrix.shape[1]), v) == None:
-            e = G.add_edge(v, G.vertex(coord2(topright, tdmatrix.shape[1])))
-            weight[e] = (diff[v] + diff[G.vertex(coord2(topright, tdmatrix.shape[1]))])
-    if i+1 < tdmatrix.shape[0] and j-1 > -1:
-        if G.edge(coord2(bottomleft, tdmatrix.shape[1]), v) == None:
-            e = G.add_edge(v, G.vertex(coord2(bottomleft, tdmatrix.shape[1])))
-            weight[e] = (diff[v] + diff[G.vertex(coord2(bottomleft, tdmatrix.shape[1]))])
-    if i+1 < tdmatrix.shape[0] and j+1 < tdmatrix.shape[1]:
-        if G.edge(coord2(bottomright, tdmatrix.shape[1]), v) == None:
-            e = G.add_edge(v, G.vertex(coord2(bottomright, tdmatrix.shape[1])))
-            weight[e] = (diff[v] + diff[G.vertex(coord2(bottomright, tdmatrix.shape[1]))])
-
-vfcolor = G.new_vertex_property("vector<double>")
-ecolor = G.new_edge_property("vector<double>")
-ewidth = G.new_edge_property("int")
-for v in G.vertices():
-    vfcolor[v] = [0, 0.0, 0.0, 1.0]
-for e in G.edges():
-    ewidth[e] = weight[e]/16 + 1
-    ecolor[e] = [0.179, 0.203, 0.210, 0.8]
-
-draw.graph_draw(G, pos=pos2, output_size=(1200, 1200), vertex_fill_color=vfcolor,\
-                edge_color=ecolor, edge_pen_width=ewidth, output="tdg.png")
+quit()
 
 source = G.vertex(coord2((12, 1), tdmatrix.shape[1]))
 target = G.vertex(coord2((4, 14), tdmatrix.shape[1]))
 
-dist, pred = search.dijkstra_search(G, weight, source, Visitor())
+dist, pred = search.dijkstra_search(G, G.ep.weight, source, Visitor())
 
 v = target
-vfcolor[v] = [0.640625, 0, 0, 0.9]
+G.vp.vfcolor[v] = [0.640625, 0, 0, 0.9]
 while v != source:
     p = G.vertex(pred[v])
     for e in v.out_edges():
         if e.target() == p:
-            ecolor[e] = [0.640625, 0, 0, 0.9]
-            ewidth[e] = 16
+            G.ep.ecolor[e] = [0.640625, 0, 0, 0.9]
+            G.ep.ewidth[e] = 16
     v = p
-    vfcolor[v] = [0.640625, 0, 0, 0.9]
+    G.vp.vfcolor[v] = [0.640625, 0, 0, 0.9]
 
-draw.graph_draw(G, pos=pos2, output_size=(1200, 1200), vertex_fill_color=vfcolor,\
-                edge_color=ecolor, edge_pen_width=ewidth, output="dijkstra.png")
+draw.graph_draw(G, pos=G.vp.pos2, output_size=(1200, 1200), vertex_fill_color=G.vp.vfcolor,\
+                edge_color=G.ep.ecolor, edge_pen_width=G.ep.ewidth, output="dijkstra.png")
